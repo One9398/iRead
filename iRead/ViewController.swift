531e5b6f7ad6ed2e1394a20b231330fdfea692e3
Simon <oneSimon9398@gmail.com>
2016-03-02 14:46:32 +0800

    Initial Commit

---
+++ iRead/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iRead
+//
+//  Created by Simon on 16/3/2.
+//  Copyright © 2016年 Simon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
