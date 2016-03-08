a5b992bcc8c8d752d7da63b1bead454b3702e0cd
Simon <oneSimon9398@gmail.com>
2016-03-08 14:26:05 +0800

    整体架构划分: 推荐RSS, 收藏/消息, RSS笔记, 用户界面

---
+++ iRead/ViewControllers/NotesViewController/NotesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NotesViewController.swift
+//  iRead
+//
+//  Created by Simon on 16/3/8.
+//  Copyright © 2016年 Simon. All rights reserved.
+//
+
+import UIKit
+
+class NotesViewController: UIViewController {
+
+}
---
+++ iRead/ViewControllers/RecommendController/RecommendController.swift
@@ -0,0 +1,13 @@
+//
+//  RecommendController.swift
+//  iRead
+//
+//  Created by Simon on 16/3/8.
+//  Copyright © 2016年 Simon. All rights reserved.
+//
+
+import UIKit
+
+class RecommendController: UIViewController {
+
+}
